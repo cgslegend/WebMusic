e148b85458818f32d39a8bdcd00f9c8b5d2d76b2
cgslegend <718827641@qq.com>
2022-06-11 19:53:29 +0800

    commit

--- dist/javascript.19a21263.js
+++ dist/javascript.19a21263.js
@@ -530,7 +530,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "13074" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "12364" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
